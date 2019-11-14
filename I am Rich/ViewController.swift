424cdc2921e52cc854264ebbab603f9ef8a0e7e8
Vitor Hugo S <vitor.h8@icloud.com>
2019-11-14 09:50:40 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Vitor Hugo on 11/14/19.
+//  Copyright © 2019 Vitor Hugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
